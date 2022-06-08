1a6a0791d768e103a722277ca5fd8d99b96d7fe6
Luiz Felipe Canário Costa <luizfelipecanariocosta@gmail.com>
2022-06-08 20:58:08 -0300

    Js

---
+++ Recaptulação.js
@@ -0,0 +1,34 @@
+const nummero1 =1;
+const nummero2 =2;
+
+console.log("A soma de " + nummero1 + " e " + nummero2 + " é " + (nummero1 + nummero2));
+
+console.log(`A soma de ${nummero1} e ${nummero2} é: ${nummero1+nummero2}`);
+
+//------------------------------------------
+
+const array = [1, "Fodase", {a:3}, [4,5]];
+
+console.log(array[1]);
+
+//------------------------------------------
+
+const palavra = 'pitchuca'; 
+console.log(palavra.length);
+console.log(palavra.replace("tchuca","tchuquinha"));
+
+
+const arraynew = [1 ,2 , 3];
+console.log(arraynew.length);
+console.log(arraynew.join("_"));
+
+
+
+//----------------------------------------
+
+
+
+
+
+
+
