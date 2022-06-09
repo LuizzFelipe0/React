f96833e12ee1acbf62740ac3cc7830836e098296
Luiz Felipe Canário Costa <luizfelipecanariocosta@gmail.com>
2022-06-08 21:29:45 -0300

    JS

--- Recaptulação.js
+++ Recaptulação.js
@@ -5,16 +5,21 @@ console.log("A soma de " + nummero1 + " e " + nummero2 + " é " + (nummero1 + nu
 
 console.log(`A soma de ${nummero1} e ${nummero2} é: ${nummero1+nummero2}`);
 
+console.log("\n");
+
 //------------------------------------------
 
 const array = [1, "Fodase", {a:3}, [4,5]];
 
 console.log(array[1]);
 
+console.log("\n");
+
 //------------------------------------------
 
 const palavra = 'pitchuca'; 
 console.log(palavra.length);
+
 console.log(palavra.replace("tchuca","tchuquinha"));
 
 
@@ -22,13 +27,38 @@ const arraynew = [1 ,2 , 3];
 console.log(arraynew.length);
 console.log(arraynew.join("_"));
 
-
+console.log("\n");
 
 //----------------------------------------
 
+/*Arrow Function*/
+
+const oi = () => {
+    console.log("Oi Luiz!")
+}
+
+oi();
+
+const homelander = (_)=>{
+    console.log(_)
+}
+
+homelander("I'm the Homelander, and I can do Whatever the Fuck I want!")
+
+console.log("\n");
+//------------------------------------
+
+const registros = [
+    {nome: 'Luiz', idade:18},
+    {nome: 'Hang', idade:12},
+    {nome: 'Cauã', idade:10},
+    {nome: 'Felipe', idade:82},
+];
+
+const idades = registros.map((registro) => registro.idade);
+console.log(idades);
 
 
-
-
-
+const nomes = registros.map((registro) => registro.nome);
+console.log(nomes);
 
